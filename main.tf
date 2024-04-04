d8475eed3fc49609a8db9c1504639fb9c4ec014e
subash2devopsintern <subaschy729@gmail.com>
2024-04-04 06:53:33 +0545

    Teraform code : Test 4.4

--- main.tf
+++ main.tf
@@ -12,35 +12,35 @@ provider "aws" {
 #   }
 # }
 
-# terraform {
-#   backend "s31" {
-#     bucket         = "dev.subash-frontend"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "subash_terraform_dynamo_db"
-#   }
-# }
-
-locals {
-  bucket_name = var.web_name == "prod.subash-frontend" ? "prod-subash-frontend-bucket" : "dev-subash-frontend-bucket"
-}
-
 terraform {
-  backend "s3" {
+  backend "s31" {
+    bucket         = "dev.subash-frontend"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "subash_terraform_dynamo_db"
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = local.bucket_name
-    key    = var.web_name == "prod.subash-frontend" ? "terraform/prod.tfstate" : "terraform/dev.tfstate"
-    region = "us-east-1"
-  }
-}
+# locals {
+#   bucket_name = var.web_name == "prod.subash-frontend" ? "prod-subash-frontend-bucket" : "dev-subash-frontend-bucket"
+# }
+
+# terraform {
+#   backend "s3" {
+#     region         = "us-east-1"
+#     dynamodb_table = "subash_terraform_dynamo_db"
+#   }
+# }
+
+# terraform {
+#   backend "s3" {
+#     bucket = local.bucket_name
+#     key    = var.web_name == "prod.subash-frontend" ? "terraform/prod.tfstate" : "terraform/dev.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 module "webapp1" {
   source   = "./modules/workflow/frontend"
-  web_name = var.web_name
+  web_name = "prod.subash-frontend"
 }
