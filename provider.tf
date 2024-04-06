fca1541e91b6a461c9fd5d76844a43ea6638711d
subash2devopsintern <subaschy729@gmail.com>
2024-04-06 14:38:59 +0545

    uploading new config

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["c:/Users/Dell/.aws/conf"]
-  shared_credentials_files = ["c:/Users/Dell/.aws/credentials"]
-  profile                  = "default"
-}
